feeb6ccef9c80dc1e92ae9aef7adf771ff22f41b
Kristal <bellettikristal@gmail.com>
2024-07-31 20:50:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 const charlas = [
+    /*BÁSICAS*/
     { area: 'Básicas', charla: 'Análisis de Fenómenos Ópticos', horario: '14:50 - 15:10', aula: 'Lab Física I', descripcion: 'Experimento en el Laboratorio de Física I de Óptica Geométrica. Estudio de la Reflexión y refracción de la luz en superficies planas y curvas.' },
-    { area: 'Básicas', charla: 'Análisis de Fenómenos Ópticos', horario: '15:20 - 15:40', aula: 'Lab Física I', descripcion: 'Experimento en el Laboratorio de Física I de Óptica Geométrica. Estudio de la Reflexión y refracción de la luz en superficies planas y curvas.' },
     { area: 'Básicas', charla: 'Análisis de Fenómenos Ópticos', horario: '16:00 - 16:20', aula: 'Lab Física I', descripcion: 'Experimento en el Laboratorio de Física I de Óptica Geométrica. Estudio de la Reflexión y refracción de la luz en superficies planas y curvas.' },
-    { area: 'Básicas', charla: 'Análisis de Fenómenos Ópticos', horario: '16:30 - 16:50', aula: 'Lab Física I', descripcion: 'Experimento en el Laboratorio de Física I de Óptica Geométrica. Estudio de la Reflexión y refracción de la luz en superficies planas y curvas.' },
     { area: 'Básicas', charla: 'Análisis de Fenómenos Ópticos', horario: '17:00 - 17:20', aula: 'Lab Física I', descripcion: 'Experimento en el Laboratorio de Física I de Óptica Geométrica. Estudio de la Reflexión y refracción de la luz en superficies planas y curvas.' },
+    { area: 'Básicas', charla: 'Péndulo Balístico', horario: '15:20 - 15:40', aula: 'Lab Física I', descripcion: 'Determinación de la rapidez de lanzamiento de un proyectil por medio de un péndulo balístico.' },
+    { area: 'Básicas', charla: 'Péndulo Balístico', horario: '16:00 - 16:20', aula: 'Lab Física I', descripcion: 'Determinación de la rapidez de lanzamiento de un proyectil por medio de un péndulo balístico.' },
+    { area: 'Básicas', charla: 'Péndulo Balístico', horario: '16:30 - 16:50', aula: 'Lab Física I', descripcion: 'Determinación de la rapidez de lanzamiento de un proyectil por medio de un péndulo balístico.' },
+    { area: 'Básicas', charla: 'Péndulo Balístico', horario: '17:00 - 17:20', aula: 'Lab Física I', descripcion: 'Determinación de la rapidez de lanzamiento de un proyectil por medio de un péndulo balístico.' },
+    { area: 'Básicas', charla: 'Visualización de Carga y descarga de un capacitor', horario: '15:20 - 15:40', aula: 'Lab Física 2', descripcion: 'Experiencia en el Laboratorio de Física II.' },
+    { area: 'Básicas', charla: 'Visualización de Carga y descarga de un capacitor', horario: '16:30 - 16:50', aula: 'Lab Física 2', descripcion: 'Experiencia en el Laboratorio de Física II.' },
+    { area: 'Básicas', charla: 'Visualización de Carga y descarga de un capacitor', horario: '17:30 - 17:50', aula: 'Lab Física 2', descripcion: 'Experiencia en el Laboratorio de Física II.' },
+    { area: 'Básicas', charla: 'Introducción al diseño 3D', horario: '15:20 - 15:40', aula: 'Lab Física 1', descripcion: 'Introducción al diseño 3D' },
+    { area: 'Básicas', charla: 'Introducción al diseño 3D', horario: '16:30 - 16:50', aula: 'Lab Física 1', descripcion: 'Introducción al diseño 3D' },
+    { area: 'Básicas', charla: 'Introducción al diseño 3D', horario: '17:30 - 17:50', aula: 'Lab Física 1', descripcion: 'Introducción al diseño 3D' },
+
+/*CIVIL*/
+    
     { area: 'Civil', charla: 'Lab. de Informática ', horario: '16:00 - 16:20', aula: 'Anfiteatro', descripcion: 'Se presentaran los Sofware que maneja un ingeniero civil a lo largo de su profesión. Herramienta fundamentas para Diseño, Dibujo y Calculo. ' },
-    { area: 'Eléctrica', charla: 'Captura y visualización de datos para sistemas de generación de Energías Renovables', horario: '16:30 - 16:50', aula: '017', descripcion: 'Sin descripción' },
+    { area: 'Civil', charla: 'Lab. de Informática ', horario: '17:30 - 17:50', aula: 'Anfiteatro', descripcion: 'Se presentaran los Sofware que maneja un ingeniero civil a lo largo de su profesión. Herramienta fundamentas para Diseño, Dibujo y Calculo. ' },
+    { area: 'Civil', charla: 'Lab. de Informática ', horario: '16:30 - 16:50', aula: 'Anfiteatro', descripcion: 'Se presentaran los Sofware que maneja un ingeniero civil a lo largo de su profesión. Herramienta fundamentas para Diseño, Dibujo y Calculo. ' },
+    { area: 'Civil', charla: 'Lab. de Informática ', horario: '17:00 - 17:20', aula: 'Anfiteatro', descripcion: 'Se presentaran los Sofware que maneja un ingeniero civil a lo largo de su profesión. Herramienta fundamentas para Diseño, Dibujo y Calculo. ' },
+    { area: 'Civil', charla: 'Lab. de Hidráulica  ', horario: '15:20 - 15:50', aula: 'Planta Piloto (enfrente)', descripcion: 'Canal Hidráulico' },
+    { area: 'Civil', charla: 'Lab. de Hidráulica  ', horario: '16:00 - 16:20', aula: 'Planta Piloto (enfrente)', descripcion: 'Canal Hidráulico' },
+    { area: 'Civil', charla: 'Lab. de Hidráulica  ', horario: '16:30 - 16:50', aula: 'Planta Piloto (enfrente)', descripcion: 'Canal Hidráulico' },
+    { area: 'Civil', charla: 'Lab. de Hidráulica  ', horario: '17:00 - 17:20', aula: 'Planta Piloto (enfrente)', descripcion: 'Canal Hidráulico' },
+    { area: 'Civil', charla: 'Lab. de ensayos', horario: '15:20 - 15:50', aula: 'Planta Piloto (enfrente)', descripcion: 'Ensayo de tracción en barra de acero y en probeta de hormigón' },
+    { area: 'Civil', charla: 'Lab. de ensayos', horario: '16:00 - 16:20', aula: 'Planta Piloto (enfrente)', descripcion: 'Ensayo de tracción en barra de acero y en probeta de hormigón' },
+    { area: 'Civil', charla: ' Perfil del profesional de la Ingeniería Civil', horario: '14:50 - 15:10', aula: 'Anfiteatro', descripcion: 'Presentación de la carrera de Ingeniería Civil. Características generales del cursado y de la actividad profesional Perfil del profesional de la Ingeniería en Civil. Actividades reservadas.' },
+    { area: 'Civil', charla: ' Perfil del profesional de la Ingeniería Civil', horario: '15:20 - 15:40', aula: 'Anfiteatro', descripcion: 'Presentación de la carrera de Ingeniería Civil. Características generales del cursado y de la actividad profesional Perfil del profesional de la Ingeniería en Civil. Actividades reservadas.' },
+ 
+/*ELECTRICA*/
+  {area: 'Eléctrica', charla: 'Captura y visualización de datos para sistemas de generación de Energías Renovables', horario: '16:00 - 16:50', aula: '017', descripcion: 'Sin descripción' },
+  {area: 'Eléctrica', charla: ' Perfil del profesional de la Ingeniería en Energía Eléctrica', horario: '15:20 - 15:40', aula: '017', descripcion: 'Sin descripción' },
+    
+/*INGRESO*/    
+    
     { area: 'Ingreso', charla: 'Arduino', horario: '16:00 - 16:20', aula: 'SUM', descripcion: 'Tablero de juegos, Simón dice, Sensor de CO, Sensor de movimiento' },
+    { area: 'Ingreso', charla: 'Arduino', horario: '16:30 - 16:50', aula: 'SUM', descripcion: 'Tablero de juegos, Simón dice, Sensor de CO, Sensor de movimiento' },
+    { area: 'Ingreso', charla: 'Arduino', horario: '17:30 - 17:50', aula: 'SUM', descripcion: 'Tablero de juegos, Simón dice, Sensor de CO, Sensor de movimiento' },
+    { area: 'Ingreso', charla: 'Test Vocacional', horario: '17:30 - 17:50', aula: 'SUM', descripcion: 'Estudiantes de Ingenieria en Sistemas programaro el Test del doctor Holland, denominado Test Chaside  que consta de 98 preguntas enfocadas a identificar tus intereses y aptitudes.' },
+    { area: 'Ingreso', charla: 'Test Vocacional', horario: '16:00 - 16:20', aula: 'SUM', descripcion: 'Estudiantes de Ingenieria en Sistemas programaro el Test del doctor Holland, denominado Test Chaside  que consta de 98 preguntas enfocadas a identificar tus intereses y aptitudes.' },
+    { area: 'Ingreso', charla: 'Test Vocacional', horario: '16:30 - 16:50', aula: 'SUM', descripcion: 'Estudiantes de Ingenieria en Sistemas programaro el Test del doctor Holland, denominado Test Chaside  que consta de 98 preguntas enfocadas a identificar tus intereses y aptitudes.' },
+    
     // Agrega más charlas según sea necesario
 ];
 
